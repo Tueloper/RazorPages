9fdcfd90be10445ba8cb40a73c25885aed67814b
Tochukwu Ozurumba <ozurumbatochukwu@yahoo.com>
2022-07-21 23:12:40 -0400

    Razor page added, layout link added and name information inputed

---
+++ RazorWebPages/Pages/Intro.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorWebPages.Pages
+{
+    public class IntroductionModel : PageModel
+    {
+        public void Intro()
+        {
+        }
+    }
+}
+
